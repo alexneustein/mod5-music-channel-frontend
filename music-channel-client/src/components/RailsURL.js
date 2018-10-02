3c5e25dda44fc19dc1b641d677d4c6bd932c820e
Alex Neustein <alex.neustein@flatironschool.com>
2018-10-02 19:27:06 -0400

    updating

--- music-channel-client/src/components/RailsURL.js
+++ music-channel-client/src/components/RailsURL.js
@@ -1,7 +1,7 @@
-export const RAILS_URL = "http://localhost:3001"
-export const WS_URL = "ws://localhost:3001"
-export const RAILS_USER = "1"
+// export const RAILS_URL = "http://localhost:3001"
+// export const WS_URL = "ws://localhost:3001"
+// export const RAILS_USER = "1"
 
-// export const RAILS_URL = "http://10.39.111.2:3001"
-// export const WS_URL = "ws://10.39.111.2:3001"
-// export const RAILS_USER = "2"
+export const RAILS_URL = "http://10.39.111.2:3001"
+export const WS_URL = "ws://10.39.111.2:3001"
+export const RAILS_USER = "2"
