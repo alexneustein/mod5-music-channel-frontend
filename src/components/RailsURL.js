ebc36e3c2ef0cf9066035e287e745a708c2174e4
Alex Neustein <alex.neustein@gmail.com>
2019-01-13 18:07:41 -0500

    updated URLs

--- src/components/RailsURL.js
+++ src/components/RailsURL.js
@@ -1,6 +1,12 @@
-export const RAILS_URL = "http://localhost:3001"
-export const WS_URL = "ws://localhost:3001"
+export const RAILS_URL = "https://pianocast-backend.herokuapp.com"
+export const WS_URL = "ws://pianocast-backend.herokuapp.com"
 export const RAILS_USER = "1"
+
+// export const RAILS_URL = "http://localhost:3001"
+// export const WS_URL = "ws://localhost:3001"
+// export const RAILS_USER = "1"
+
+
 
 // export const RAILS_URL = "http://10.39.111.2:3001"
 // export const WS_URL = "ws://10.39.111.2:3001"
